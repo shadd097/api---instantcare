45c63d481bdf5806caf8596499221bd430101de3
shadd097 <84427267+shadd097@users.noreply.github.com>
2023-12-10 22:39:14 -0500

    delete nodemodules

---
+++ config/cloudinary.js
@@ -0,0 +1,7 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dzue43fjc', 
+  api_key: '624366772534191', 
+  api_secret: 'a7Iu3Ww8XzD0jFKoz-0n41CqOB4' 
+});
